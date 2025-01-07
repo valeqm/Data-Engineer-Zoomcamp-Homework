7c2422593396f6555cfe410c2ab557f70da69a4c
valeqm <valeriaqm_24_11@hotmail.com>
2025-01-07 02:01:59 -0500

    Homework

--- Homework_01/ingest_data.py
+++ Homework_01/ingest_data.py
@@ -23,44 +23,52 @@ def main(params):
     # for pandas to be able to open the file
     if url.endswith('.csv.gz'):
         csv_name = 'output.csv.gz'
+        mode=1
     else:
-        csv_name = 'output.csv'
+        csv_name = 'output2.csv'
+        mode=2
 
     os.system(f"wget {url} -O {csv_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    if mode==1:
+        df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
-    df = next(df_iter)
+        df = next(df_iter)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+        df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+        df.to_sql(name=table_name, con=engine, if_exists='append')
 
 
-    while True: 
+        while True: 
 
-        try:
-            t_start = time()
-            
-            df = next(df_iter)
+            try:
+                t_start = time()
+                
+                df = next(df_iter)
 
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+                df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+                df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
-            df.to_sql(name=table_name, con=engine, if_exists='append')
+                df.to_sql(name=table_name, con=engine, if_exists='append')
 
-            t_end = time()
+                t_end = time()
 
-            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+                print('inserted another chunk, took %.3f second' % (t_end - t_start))
 
-        except StopIteration:
-            print("Finished ingesting data into the postgres database")
-            break
+            except StopIteration:
+                print("Finished ingesting data into the postgres database")
+                break
+    else:
+        df_zone = pd.read_csv(csv_name)
+        df_zone.to_sql(name=table_name, con=engine, if_exists='replace')
+        print('Finish data zones')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
