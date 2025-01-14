7afd2eaa55c0a15a421557a5fbaed3a7067cc626
valeqm <valeriaqm_24_11@hotmail.com>
2025-01-14 12:21:45 -0500

    Homework update 2025

--- Homework_01/SQL_Queries.sql
+++ Homework_01/SQL_Queries.sql
@@ -1,7 +1,13 @@
 -- Question 3
-select count(*) as total
+select
+  count(case when trip_distance <= 1 then 1 end) as "1",
+  count(case when trip_distance > 1 and trip_distance <= 3 then 1 end) as "2",
+  count(case when trip_distance > 3 and trip_distance <= 7 then 1 end) as "3",
+  count(case when trip_distance > 7 and trip_distance <= 10 then 1 end) as "4",
+  count(case when trip_distance > 10 then 1 end) as "5"
 from green_taxi_trips
-where date(lpep_pickup_datetime) = '2019-09-18' and date(lpep_dropoff_datetime) = '2019-09-18';
+where date(lpep_pickup_datetime) >= '2019-10-01' and date(lpep_pickup_datetime) < '2019-11-01'
+and date(lpep_dropoff_datetime) >= '2019-10-01' and date(lpep_dropoff_datetime) < '2019-11-01';
 
 -- Question 4
 select max(trip_distance),date(lpep_pickup_datetime)
@@ -11,13 +17,13 @@ order by max(trip_distance) desc
 limit 1;
 
 -- Question 5
-select zl."Borough", sum(gtt.total_amount) as total
+select zl."Zone" , sum(gtt.total_amount) as total
 from green_taxi_trips gtt 
 inner join zone_lookup zl 
 on gtt."PULocationID" = zl."LocationID"
-where date(gtt.lpep_pickup_datetime)='2019-09-18' and zl."Borough" != 'Unknown'
-group by zl."Borough" 
-having sum(gtt.total_amount)>50000
+where date(gtt.lpep_pickup_datetime)='2019-10-18' and zl."Zone" != 'Unknown'
+group by zl."Zone" 
+having sum(gtt.total_amount)>13000
 order by total desc
 limit 3;
 
@@ -28,6 +34,6 @@ inner join zone_lookup zl_pu
 	on gtt."PULocationID" = zl_pu."LocationID"
 inner join zone_lookup zl_do 
 	on gtt."DOLocationID" = zl_do."LocationID"
-where TO_CHAR(gtt.lpep_pickup_datetime, 'YYYY-MM') = '2019-09' and zl_pu."Zone"='Astoria'
+where TO_CHAR(gtt.lpep_pickup_datetime, 'YYYY-MM') = '2019-10' and zl_pu."Zone"='East Harlem North'
 order by gtt.tip_amount desc
 limit 1;
--- Homework_01/terraform/variables.tf
+++ Homework_01/terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "<Path to your Service Account json file>"
+  default     = "./keys/my-creds.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
@@ -8,7 +8,7 @@ variable "credentials" {
 
 variable "project" {
   description = "Project"
-  default     = "<Your Project ID>"
+  default     = "woven-nova-447816-e0"
 }
 
 variable "region" {
@@ -26,13 +26,13 @@ variable "location" {
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
   #Update the below to what you want your dataset to be called
-  default     = "demo_dataset"
+  default     = "terraform_dataset"
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+  default     = "terraform-woven-nova-447816-e0-bucket"
 }
 
 variable "gcs_storage_class" {
